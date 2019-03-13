d5d30fc37b74dbc39aa82a235a5ae9e82878b485
Naudria <rima.jean@gmail.com>
2019-03-13 11:41:43 -0500

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,14 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
-  // Write your JavaScript code inside the init() function
-
+    let index = 0;
+    document.body.addEventListener('keydown', function(k) {
+        const keyPressed = parseInt(k.detail || k.which);
+        if (code[index] === keyPressed) {
+            index++;
+            alert('Sequence is Konami');
+        } else {
+            index = 0;
+        }
+    });
 }
